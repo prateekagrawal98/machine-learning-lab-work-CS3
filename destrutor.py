23c753d923c0d89cb3cf6a3288d05b6ed14b37fd
prateekagrawal98 <48116739+prateekagrawal98@users.noreply.github.com>
2019-03-15 12:23:45 +0530

    Create destrutor.py

---
+++ destrutor.py
@@ -0,0 +1,8 @@
+class Employee:
+    
+    def __init__(self):
+              print('employee created')
+    def __del__(self):
+           print('destructor called,employee deleted')
+employee=Employee()
+del employee
